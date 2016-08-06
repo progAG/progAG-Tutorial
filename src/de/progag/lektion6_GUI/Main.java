028cfb8b1f896908a6c5abf6ba9f14747da50c95
DrBackmischung <mathis.neunzig@gmail.com>
2016-08-07 01:08:26 +0200

    Letion 6 angefangen

--- src/de/progag/lektion6_GUI/Main.java
+++ src/de/progag/lektion6_GUI/Main.java
@@ -1,5 +1,23 @@
 package de.progag.lektion6_GUI;
 
-public class Main {
+import javax.swing.JFrame;
 
+public class Main extends JFrame {
+	
+	public static void main(String[] args) {
+		
+		new Main();
+		
+	}
+	
+	public Main() {
+		
+		setDefaultCloseOperation(EXIT_ON_CLOSE); // Beim Schlieﬂen soll das Programm sich beenden.
+		setExtendedState(MAXIMIZED_BOTH); // Das Programm wird maximiert gestartet.
+		setLayout(null); // Es wird kein layout verwendet. Es gibt vorgegebene, jedoch kann man ohne besser arbeiten.
+		
+		setVisible(true); // Letzter Befehl. Das JFrame wird angezeigt.
+		
+	}
+	
 }
