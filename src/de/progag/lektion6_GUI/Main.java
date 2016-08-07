69bd9184b35d6f11cb56342fa1a5d41608542d0c
DrBackmischung <mathis.neunzig@gmail.com>
2016-08-07 10:03:06 +0200

    Lektion 6 fertiggestellt

--- src/de/progag/lektion6_GUI/Main.java
+++ src/de/progag/lektion6_GUI/Main.java
@@ -4,13 +4,31 @@ import javax.swing.JFrame;
 
 public class Main extends JFrame {
 	
+	/**
+	 * Eine GUI ist ein Benutzerinterface. Allgemein gesagt ist es ein Fenster, was sich öffnet.
+	 * Java hat dafür eine Bibliothek namens "Swing".
+	 * Ein leeres Fenster, was man öffnen kann, heißt "JFrame" und ist ein Objekt, welches wir brauchen.
+	 * Da wir ein neues Fenster (GUI) erstellen, müssen wir hinter "public claas Main" noch "extends JFrame" schreiben, was besagt, dass wir dieses Fenster "kopieren".
+	 * Diese Klasse hat jetzt alle Eigenschaften eines JFrames. Man kann es also schon theoretisch öffnen.
+	 */
+	
 	public static void main(String[] args) {
+		
+		/**
+		 * Für ein JFrame muss man Einstellungen vornehmen. Dieses können wir nicht in der Haupt-Methode tun.
+		 * Dafür erstellen wir diese Klasse neu.
+		 */
 		
 		new Main();
 		
 	}
 	
 	public Main() {
+		
+		/**
+		 * Einige Einstellungen sind sehr wichtig. 
+		 * Wenn man ein JFrame schließt, stoppt das Programm nicht automatisch. Außerdem ist es sehr klein.
+		 */
 		
 		setDefaultCloseOperation(EXIT_ON_CLOSE); // Beim Schließen soll das Programm sich beenden.
 		setExtendedState(MAXIMIZED_BOTH); // Das Programm wird maximiert gestartet.
